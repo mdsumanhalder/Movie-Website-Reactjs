b31d100bd08e56306a64f38dade3364f8a33a7fb
Suman Halder <49319652+programmersumanhalder@users.noreply.github.com>
2021-01-05 12:36:11 +0200

    Add files via upload

---
+++ precache-manifest.f9b7c1113af96c49152036d71a4a5072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf267ad5d42ba2ef7e57c7ceb2b5c822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ad891b185aa1f13da0",
+    "url": "/static/css/main.7b3c70f0.chunk.css"
+  },
+  {
+    "revision": "d32475b03f10990432f7",
+    "url": "/static/js/2.b612a5f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b612a5f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ad891b185aa1f13da0",
+    "url": "/static/js/main.5af5e8e1.chunk.js"
+  },
+  {
+    "revision": "24adc4d9deed8b5d4b35",
+    "url": "/static/js/runtime-main.11124dbc.js"
+  }
+]);
